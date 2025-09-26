60c8f9b21bc7814eeb0be8dbcdb97aef9cc206ed
NICO BODER <nico.boder@mercedes-benz.com>
2025-09-26 17:49:13 +0200

    Removed unused LeagueManagement

--- libs/league-management/README.md
+++
@@ -1,7 +0,0 @@
-# LeagueManagement
-
-This library was generated with [Nx](https://nx.dev).
-
-## Running unit tests
-
-Run `nx test LeagueManagement` to execute the unit tests.
--- libs/league-management/src/lib/LeagueManagement/LeagueManagement.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'lib-league-management',
-  imports: [],
-  templateUrl: './LeagueManagement.html',
-  styleUrl: './LeagueManagement.scss',
-})
-export class LeagueManagement {}
