de247dc7b3ef080fcc54834bb08c88834c50339e
NICO BODER <nico.boder@mercedes-benz.com>
2025-09-26 11:06:05 +0200

    add basic setup for showing Login.

--- libs/login/src/lib/login/login.ts
+++ libs/login/src/lib/login/login.ts
@@ -1,9 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { MatFormField, MatLabel } from '@angular/material/form-field';
+import { MatInput } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'lib-login',
-  imports: [],
+  imports: [MatFormField, MatLabel, MatInput, FormsModule],
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
-export class Login {}
+export class Login {
+  username = signal<string>('');
+  password = signal<string>('');
+  loginUser() {
+    console.log("login called")
+    console.log("Username: " + this.username())
+    console.log("Password: " + this.password())
+  }
+}
