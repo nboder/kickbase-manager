50e161849d6a54d5e6ad9522ea86880fe95be369
NICO BODER <nico.boder@mercedes-benz.com>
2025-10-06 12:36:07 +0200

    #17: Start implementing the Squad Tile

--- libs/staff-planing/src/lib/squad-player-card/SquadPlayerCard.ts
+++ libs/staff-planing/src/lib/squad-player-card/SquadPlayerCard.ts
@@ -1,11 +1,25 @@
 import { Component, input } from '@angular/core';
+import { MatCard, MatCardContent, MatCardFooter, MatCardHeader, MatCardSubtitle, MatCardTitle } from '@angular/material/card';
+import { PositionMarker } from '@kickbase/PositionMarker';
+import { KickbaseStaffPosition, MoneyPipe } from '@kickbase/definitions';
 
 @Component({
   selector: 'lib-squad-player-card',
-  imports: [],
+  imports: [
+    MatCard,
+    MatCardHeader,
+    MatCardFooter,
+    PositionMarker,
+    MatCardTitle,
+    MatCardSubtitle,
+    MoneyPipe,
+    MatCardContent,
+  ],
   templateUrl: './SquadPlayerCard.html',
   styleUrl: './SquadPlayerCard.scss',
 })
 export class SquadPlayerCard {
   playerName = input.required<string>();
+  marketValue = input.required<number>();
+  position = input.required<KickbaseStaffPosition>();
 }
