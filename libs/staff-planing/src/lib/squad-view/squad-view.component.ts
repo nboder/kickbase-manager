2794998c2bc6e33bbafacf57f9b4055c6320b97a
NICO BODER <nico.boder@mercedes-benz.com>
2025-10-09 09:24:32 +0200

    #23 Separated Squad into single Component.

---
+++ libs/staff-planing/src/lib/squad-view/squad-view.component.ts
@@ -0,0 +1,91 @@
+import { Component, computed, inject, OnInit, signal } from '@angular/core';
+import { MatButton } from '@angular/material/button';
+import { SquadPlayerCard } from '../squad-player-card/SquadPlayerCard';
+import {
+  KickbaseLeagueConstants,
+  Player,
+  SquadResponseStaff,
+} from '@kickbase/definitions';
+import { StaffPlaningService } from '../service/staff-planing-service';
+import {
+  LocalStoragePersistenceManager,
+  PersistenceManager,
+  SellingPlayer,
+} from '@kickbase/persistence-management';
+
+@Component({
+  selector: 'lib-sqaud-view',
+  imports: [MatButton, SquadPlayerCard],
+  templateUrl: './squad-view.component.html',
+  styleUrl: './squad-view.component.scss',
+})
+export class SquadView implements OnInit {
+  sumOfSOldPlayers = computed(() => {
+    const soldPlayer = this.mySquad().filter((mySquadPlayer) =>
+      this.storageManager.containedInPlayersToSell(mySquadPlayer.playerId)
+    );
+    let sumOfSellingPlayers = 0;
+    soldPlayer.forEach((player) => {
+      sumOfSellingPlayers += player.marketValue;
+    });
+    return sumOfSellingPlayers;
+  });
+
+  twentyFourHourMarketValuePredictions = computed(() => {
+    return this.mySquad()
+      .filter((mySquadPlayer) => !this.isPlayerMarkedForSelling(mySquadPlayer))
+      .map((value) => value.twentyForHoursDevelopment);
+  });
+
+  sevenDayMarketValuePredictions = computed(() => {
+    return this.mySquad()
+      .filter((mySquadPlayer) => !this.isPlayerMarkedForSelling(mySquadPlayer))
+      .map((value) => value.sevenDayPrediction);
+  });
+
+  private readonly storageManager: PersistenceManager = inject(
+    LocalStoragePersistenceManager
+  );
+  private readonly staffService = inject(StaffPlaningService);
+  mySquad = signal<Player[]>([]);
+
+  ngOnInit(): void {
+    this.storageManager.loadPlayersToSell();
+    this.staffService
+      .fetchMyTeam(KickbaseLeagueConstants.STROHGAEU_BUBEN_LEAGUE_ID.toString())
+      .subscribe({
+        next: (data: SquadResponseStaff) => {
+          const players = data.it.map((value) => new Player(value));
+          const sortedPlayers = players.sort((a, b) => a.position - b.position);
+          this.mySquad.set(sortedPlayers);
+        },
+        error: (err) => {
+          console.log(err);
+        },
+      });
+  }
+
+  isPlayerMarkedForSelling(player: Player): boolean {
+    return this.storageManager.containedInPlayersToSell(player.playerId);
+  }
+
+  playerToggleSellStatus(playerId: string, shouldBeSold: boolean) {
+    if (shouldBeSold) {
+      this.storageManager.storeSellablePlayer(new SellingPlayer(playerId));
+    } else {
+      this.storageManager.removeSellablePlayer(new SellingPlayer(playerId));
+    }
+  }
+
+  sellAllPlayersNotInSquad() {
+    this.mySquad().forEach((player) => {
+      if (!player.isInSquad) {
+        this.storageManager.storeSellablePlayer(player);
+      }
+    });
+  }
+
+  resetAllSellingCandidates() {
+    this.storageManager.clearAllSellablePlayers();
+  }
+}
